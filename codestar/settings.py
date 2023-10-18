e8ad9ca2f5754ba01fbd1d0872e8f7bc25c3c022
Oisín Tohak <oisintohak@gmail.com>
2023-10-18 15:57:35 +0100

    Update settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ["django-cloudinary-blog.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["django-cloudinary-blog.herokuapp.com", "localhost", "cloudinary-blog.onrender.com"]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
